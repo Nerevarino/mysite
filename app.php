65a2145989d84b1ed91d2bca615c5c39aa4faa4d
Evgeniy Dronov <dronov-ep@yandex.ru>
2018-08-14 14:44:04 +0300

    init

---
+++ app.php
@@ -0,0 +1,11 @@
+<?php $greeting = 'Сайт работает!'; ?>
+<!DOCTYPE html>
+<html >
+  <head >
+    <meta charset="utf-8"/>
+    <title >MySite</title>
+  </head>
+  <body >
+    <?php echo $greeting; ?>
+  </body>
+</html>
---
+++ makefile
@@ -0,0 +1,3 @@
+
+send:   ~/projects/web/mysite
+	cp -r ~/projects/web/mysite/ /var/www/
